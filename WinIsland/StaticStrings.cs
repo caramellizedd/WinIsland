97924a4b776689493a1dae44ca379f51a348fa08
caramelizedd <80157027+caramelizedd@users.noreply.github.com>
2025-12-20 12:37:29 +0700

    Changed version code.

--- WinIsland/StaticStrings.cs
+++ WinIsland/StaticStrings.cs
@@ -6,7 +6,7 @@ namespace WinIsland
 {
     public class StaticStrings
     {
-        public static String version = "0.1-PAR";
-        public static String longVersion = "Version 0.1-PublicAlphaRelease";
+        public static String version = "0.2-PAR";
+        public static String longVersion = "Version 0.2-PublicAlphaRelease";
     }
 }

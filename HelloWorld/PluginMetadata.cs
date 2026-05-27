d459a287a17a5769b7356f79ccd4dc6ea6ebd751
caramellizedd <80157027+caramellizedd@users.noreply.github.com>
2026-05-27 13:49:27 +0700

    docs: Added comments on the HelloWorld code.

--- HelloWorld/PluginMetadata.cs
+++ HelloWorld/PluginMetadata.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
 {
     public class PluginMetadata
     {
-        public static string name = "WIWeather";
-        public static string version = "0.1";
-        public static bool hasPages = true;
+        public static string name = "HelloWorld"; // The name of the plugin
+        public static string version = "0.1"; // The version of the plugin
+        public static bool hasPages = true; // Whether the plugin has pages for the island
     }
 }
--- HelloWorld/PluginPage.xaml.cs
+++ HelloWorld/PluginPage.xaml.cs
@@ -14,7 +14,15 @@ using System.Windows.Shapes;
 namespace HelloWorld
 {
     /// <summary>
-    /// Interaction logic for PluginPage.xaml
+    /// 
+    /// Welcome to the main Hello World plugin page!
+    /// This is a simple plugin that demonstrates how to create a page and basic code for WinIsland.
+    /// It serves as a starting point for your own plugin code.
+    /// You can customize these however you want.
+    /// Any external .NET libraries are supported, like EdgeWebView2 if you want to use that for some reason.
+    /// 
+    /// TODO: Create a code-only version of the plugin.
+    /// 
     /// </summary>
     public partial class PluginPage : Page
     {
